bf618920d3a7bd29600fe73a45b11f40939ebabd
yokiharo <yokiharo@gmail.com>
2016-11-17 22:29:13 +0000

    simple formatting

--- public/assets/js/nunogois.js
+++ public/assets/js/nunogois.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#CurrentAge').text(CurrentAge('1989-12-27'));
   $('.CurrentYear').text(new Date().getFullYear());
 
-  $(".page-scroll").click(function(event){     
+  $(".page-scroll").click(function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     $(this).blur();
@@ -12,15 +12,15 @@ $(document).ready(function() {
 });
 
 function CurrentAge (birthDate) {
-    var birthDate = new Date(birthDate);
-    var today = new Date();
+  var birthDate = new Date(birthDate);
+  var today = new Date();
 
-    var years = (today.getFullYear() - birthDate.getFullYear());
+  var years = (today.getFullYear() - birthDate.getFullYear());
 
-    if (today.getMonth() < birthDate.getMonth() ||
-        today.getMonth() == birthDate.getMonth() && today.getDate() < birthDate.getDate()) {
-        years--;
-    }
+  if (today.getMonth() < birthDate.getMonth() ||
+  today.getMonth() == birthDate.getMonth() && today.getDate() < birthDate.getDate()) {
+    years--;
+  }
 
-    return years;
+  return years;
 }
