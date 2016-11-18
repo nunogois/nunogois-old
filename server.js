1e5290d5504081ad29789bb168074c8a33ea271f
Nuno Góis <yokiharo@gmail.com>
2016-11-18 10:27:22 +0000

    better comments

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-// Main objects...
+// main variables
 var express = require('express');
 var app = express();
 var http = require('http');
 
 var path = require('path');
 
+// set public folder for assets
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Index page
+// route index page
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// Start server ...
-http.Server(app).listen(process.env.PORT || 80, function(){
+// start server
+http.Server(app).listen(process.env.PORT || 80, function() {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
