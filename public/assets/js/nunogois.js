4d4d4e9a359e046d5553c02be208e2f2a72977c6
yokiharo <yokiharo@gmail.com>
2016-11-21 17:41:15 +0000

    contact form added using mailgun
    contact form added using mailgun

--- public/assets/js/nunogois.js
+++ public/assets/js/nunogois.js
@@ -9,8 +9,11 @@ $(document).ready(function() {
     $(this).blur();
   });
 
+  HideAntiBotInput();
+  TextAreaAutoSize();
+
   EasterEgg();
-  
+
 });
 
 function CurrentAge (birthDate) {
@@ -27,7 +30,60 @@ function CurrentAge (birthDate) {
   return years;
 }
 
+function HideAntiBotInput()
+{
+  $('#name').hide();
+}
+
+function TextAreaAutoSize() {
+  $('textarea').each(function () {
+    this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;resize:none;');
+  }).on('input', function () {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+  });
+}
+
 function EasterEgg() {
   console.log("                                                                  \n       `'##+.                                                     \n     '@@@@@@@@+                                  `@@@@@@@@@@@@@@@ \n    @@@@@@@@@@@@                                 `@@@@@@@@@@@@@@@ \n   @@@@@@@@@@@@@@`                               `@@@@@@@@@@@@@@@ \n  ;@@@@@@@@@@@@@@@.                              `@@@@@@@@@@@@@@@ \n  @@@@@@@@@@@@@@@@@,                             `@@@@@@@@@@@@@@@ \n .@@@@@@@@@@@@@@@@@@;                            `@@@@@@@@@@@@@@@ \n #@@@@@@@@@@@@@@@@@@@+                           `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@#                          `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@                         `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@                        `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@                       `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@`                     `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@`                    `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@:                   `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@'                  `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+                 `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#                `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@              `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@             `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@`           `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.          `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@'@@@@@@@@@@@@@@@@@@@@@@:         `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@ :@@@@@@@@@@@@@@@@@@@@@@'        `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@  .@@@@@@@@@@@@@@@@@@@@@@#       `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@   `@@@@@@@@@@@@@@@@@@@@@@@      `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@@@@@@@@     `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@@@@@@@@    `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@@   `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@        #@@@@@@@@@@@@@@@@@@@@@@` `@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@         +@@@@@@@@@@@@@@@@@@@@@@,`@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@          '@@@@@@@@@@@@@@@@@@@@@@;@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@           :@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@            .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@             `@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                  #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                   +@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                    ;@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                     ,@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                      `@@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                       `@@@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                         @@@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                          @@@@@@@@@@@@@@@@@@@@@@@ \n @@@@@@@@@@@@@@@                           @@@@@@@@@@@@@@@@@@@@@# \n @@@@@@@@@@@@@@@                            #@@@@@@@@@@@@@@@@@@@; \n @@@@@@@@@@@@@@@                             +@@@@@@@@@@@@@@@@@@` \n @@@@@@@@@@@@@@@                              :@@@@@@@@@@@@@@@@@  \n @@@@@@@@@@@@@@@                               ,@@@@@@@@@@@@@@@.  \n @@@@@@@@@@@@@@@                                .@@@@@@@@@@@@@@   \n @@@@@@@@@@@@@@@                                  @@@@@@@@@@@@    ");
   console.log("%cNuno Góis - Software Developer", "background-color:black;color:white;font-size:26px;");
+}
+
+
+function ToggleEmail() {
+  $('.contact').slideToggle( "slow" );
+  $('.email-form').slideToggle( "slow" );
+}
+
+function SendEmail() {
+  var name = $('#name').val(); // anti-bot input
+  var contact_name = $('#contact_name').val(); // actual name input
+  var contact_email = $('#contact_email').val(); // email input
+  var contact_subject = $('#contact_subject').val(); // subject input
+  var contact_message = $('#contact_message').val(); // message textarea
+
+  if (name === "" & contact_name !== "" & contact_email !== "" & isEmail(contact_email) & contact_subject !== "" & contact_message !== "")
+  {
+    $('.btn-send').attr('disabled', true);
+    $('.btn-send').attr('onclick', false);
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify({ name: name, contact_name: contact_name, contact_email: contact_email, contact_subject: contact_subject, contact_message: contact_message }),
+      contentType: 'application/json',
+      url: 'send',
+      success: function(data) {
+        toastr.success('Email sent sucessfully! Thank you for your message.', 'Nuno Góis');
+      }
+    });
+  }
+  else
+  {
+    toastr.error('Please fill all the required fields correctly.', 'Nuno Góis');
+    $('.btn-send').attr('disabled', false);
+  }
+}
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
 }
--- server.js
+++ server.js
@@ -5,12 +5,35 @@ var http = require('http');
 
 var path = require('path');
 
+// body parser middleware
+app.use(express.bodyParser());
+
 // set public folder for assets
 app.use(express.static(path.join(__dirname, 'public')));
 
 // route index page
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+// route email sending
+app.post('/send', function(req, res){
+  var api_key = process.env.MAILGUN_API_KEY; // mailgun API key
+  var domain = 'mg.nunogois.com';
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+  var data = {
+    from: req.body.contact_name + ' <portfolio@mg.nunogois.com>',
+    to: process.env.PERSONAL_EMAIL, // my personal email address
+    subject: req.body.contact_subject,
+    text: 'From: ' + req.body.contact_name + ' <' + req.body.contact_email + '>\n\nMessage:' + req.body.contact_message;
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+
+  res.json({success : "Email sent!", status : 200});
 });
 
 // start server
